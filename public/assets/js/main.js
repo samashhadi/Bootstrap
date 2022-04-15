0938429fac43bbd4215f8d28bdd1207ff846677d
samashhadi <sa.mashhadi@yahoo.com>
2022-04-15 15:08:27 +0430

    append tasks to other info card from json

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -30,10 +30,11 @@ $.ajax({
 
 		let unresolvedTickets = result.unresolvedTickets;
 		let unresolved_tickets = $(".unresolved-tickets");
-        unresolved_tickets.find('h6.card-subtitle span').html(unresolvedTickets.group);
+        unresolved_tickets.find('h5.card-title').html(unresolvedTickets.title);
+        unresolved_tickets.find('h6.card-subtitle').html(unresolvedTickets.subtitle);
       
 		var tickets = unresolved_tickets.find('ul.list-group');
-		$.each( unresolvedTickets.tickets, function() {
+		$.each( unresolvedTickets.items, function() {
 		    var item = $(`
                 <li class="list-group-item d-flex justify-content-between align-items-center">
                     <span class="item-title">${this.title}</span>
@@ -41,6 +42,29 @@ $.ajax({
                 </li>
 		    `);
 		    tickets.append(item);
+		});
+
+        let today_tasks = result.todayTasks;
+		let tasks = $(".tasks");
+        tasks.find('h5.card-title').html(today_tasks.title);
+        tasks.find('h6.card-subtitle').html(today_tasks.subtitle);
+      
+		var tasks_list = tasks.find('ul.list-group');
+		$.each( today_tasks.items, function() {
+		    var item = $(`
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="finishCheckChecked" ${this.checked}/>
+                <label class="form-check-label" for="finishCheckChecked">
+                ${this.title}
+                </label>
+                </div>
+                <button type="button" class="btn btn-${this.bg_color} text-${this.color} fw-bold">
+                ${this.value}
+                </button>
+            </li>
+		    `);
+		    tasks_list.append(item);
 		});
 	}
 });
